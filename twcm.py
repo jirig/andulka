4c22043c42d4cb4dd0cbb471bb521bc666964738
Jirka <jirigunka@gmail.com>
2011-02-12 15:52:33 +0100

    odstraneni problemu -out of range (128) + automaticke pripojeni k twitteru  po spusteni

--- twcm.py
+++ twcm.py
@@ -12,19 +12,21 @@ consumer_key = "qkhPVwSDfFp9qUr9KlVdPA"
 consumer_secret = "mUzfFwtes7UW854B7yW5rvb6RZE7Wy8OD5DMFrq3mCA"
 
 def vypis(twitter):
-  dataList =  twitter.GetAllUsersFavs()
+  dataList =  twitter.GetHomeTimeline()
   a = ""
   for i in range(0,len(dataList)-1):
-    a += dataList[i]['user']['screen_name'] + "\n " + (unicode (dataList[i]['text'])) + "\n"
+    a += "<b>"+dataList[i]['user']['screen_name']+ "</b>" + "<br />" + ((dataList[i]['text'])) + "<br />"
     #print(dataList[i]['user']['screen_name'])
     #print(dataList[i]['text']) 
     #print a
-  print a
+  #print a
   return a
+
 def test():      
   twitter = OAuthApi(consumer_key, consumer_secret)
   temp_credentials = twitter.getRequestToken()
   twitter = OAuthApi(consumer_key, consumer_secret, OATOKEN, OATOKENSECRET)
+  print "=====================SPOJENO"
   return twitter
   
   #user_timeline = twitter.GetUserTimeline()
