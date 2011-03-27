04c8f52ce7dc42ee090ab70a672a5fe656ab4577
Jirka <jirigunka@gmail.com>
2011-03-27 15:50:34 +0200

    zakladni zobrazeni timeline jako HT

--- twcm.py
+++ twcm.py
@@ -8,6 +8,7 @@ import pprint
 import re
 import subprocess
 import simplejson
+import json
 
 OATOKEN="111682467-nymKCZGQxijr3a8U4keoiHCArRnkLqYiHjE8QwoE"
 OATOKENSECRET= "uSELZWESpSikb3Z0w8t3TGkByrlvW3kInpK7XNdV94"
@@ -23,7 +24,7 @@ def vypis(twitter, co, search = None):
 #    dataList =  twitter.GetConcretUserFavs()
   elif(co == "search"):
      dataList = twitter.GetSearchResult(search)
-#  pomocna promenna pro ulozeni a vypis statusu
+#  pomocna promenna pro ulozeni a vypis statusExpecting , delimiter: line 1 u
   a = ""
   # pomocna promenna pro slozeni json dat pro vyzualizaci pomoci thejit
  
@@ -45,7 +46,8 @@ def vypis(twitter, co, search = None):
             if(co=="myFavs"):
                 a += "<div class='statusDiv'><img src='"+dataList[i]['user']['profile_image_url']+"'/><b>"+dataList[i]['user']['screen_name']+ "</b>" + "<br />" + text + "<br /><span class='statusTimeSpan'>"+dataList[i]['created_at']+"<a href='#' onclick='unFavPy(spojeni,\""+statusid+"\")'>unFAV</a></span></div>"
             else:
-                a += "<div class='statusDiv'><img src='"+dataList[i]['user']['profile_image_url']+"'/><b>"+dataList[i]['user']['screen_name']+ "</b>" + "<br />" + text + "<br /><span class='statusTimeSpan'>"+dataList[i]['created_at']+"<a href='#' onclick='reply(spojeni,\""+dataList[i]['user']['screen_name']+"\", \""+statusid+"\")'> | Reply | </a><a href='#' onclick='newFavPy(spojeni,\""+statusid+"\")'>FAV</a></span></div>"
+                a += "<div class='statusDiv'><img src='"+dataList[i]['user']['profile_image_url']+"'/><b>"+dataList[i]['user']['screen_name']+ "</b>" + "<br />" + text + "<br /><span class='statusTimeSpan'>"+dataList[i]['created_at']+" <a href='#' onclick='reply(spojeni,\""+dataList[i]['user']['screen_name']+"\", \""+statusid+"\")'>| Reply |</a>  <a href='#' onclick='newFavPy(spojeni,\""+statusid+"\")'>FAV</a> </span></div>"
+
                
 #            a += "<div class='statusDiv'><b>"+dataList[i]['user']['screen_name']+ "</b>" + "<br />" + ((dataList[i]['text'])) + "<br /><span class='statusTimeSpan'>"+dataList[i]['created_at']+"</span></div>"
 #        except:
@@ -54,13 +56,45 @@ def vypis(twitter, co, search = None):
     #print(dataList[i]['user']['screen_name'])
     #print(dataList[i]['text']) 
     #print a
+
+
+
     
   return a
 
-def vizData():
+def vizData(twitter):
 
-    jsn ={"id": "stred00", "name": "stred","data": { "some key": "some value", "some other key": "some other value"},"children": [ {"id": "232_42", "name": "KUCHTA Band", "data": { "tsomekey":"twitter je nununu","image":"n0M.png"},"children": []}]}
+#    jsn ={"id": "stred00", "name": "stred","data": { "some key": "some value", "some other key": "some other value"},"children": [ {"id": "232_42", "name": "KUCHTA Band", "data": { "tsomekey":"twitter je nununu","image":"n0M.png"},"children": []}]}
+#    jsn = simplejson.loads('{"id": "stred00", "name": "stred","data": { "some key": "some value", "some other key": "some other value"},"children": [ {"id": "232_42", "name": "KUCHTA Band", "data": { "tsomekey":"twitter je nununu","image":"n0M.png"},"children": []}]}')
+#    jsn = simplejson.loads('{"id": "stred00", "name": "stred","data": { "some key": "some value", "some other key": "some other value"},"children": [ {"id": "232_42", "name": "KUCHTA Band", "data": { "tsomekey":"twitter je nununu","image":"n0M.png"},"children": []},'+'{"id": "stre", "name": "sted","data": { "some key": "some value", "some other key": "some other value"},"children": []}]}')
 #    return simplejson.dumps(jsn)
+    tmp_jsn = ""
+    dataList =  twitter.GetHomeTimeline()
+    for i in range(0,len(dataList)-1):
+#        try:
+            text='...'
+#            text = replaceUrl(dataList[i]['text'])
+            text = dataList[i]['text'].replace('"', '\'')
+            text = text.replace('\\', '\\\\')
+#            text = replaceUrl(text)
+            statusid = dataList[i]['id_str']
+#            text = simplejson.JSONEncoder().encode(text)
+            sname = dataList[i]['user']['screen_name']
+            imgurl = dataList[i]['user']['profile_image_url']
+#            a += "<div class='statusDiv'><img src='"+dataList[i]['user']['profile_image_url']+"'/><b>"+dataList[i]['user']['screen_name']+ "</b>" + "<br />" + text + "<br /><span class='statusTimeSpan'>"+dataList[i]['created_at']+" <a href='#' onclick='reply(spojeni,\""+dataList[i]['user']['screen_name']+"\", \""+statusid+"\")'>| Reply |</a>  <a href='#' onclick='newFavPy(spojeni,\""+statusid+"\")'>FAV</a> </span></div>"
+            if(i == len(dataList)-2):
+                tmp_jsn += '{"id":"'+statusid+'", "name":"'+sname+'","data": {"tsomekey":"'+text+'", "image":"'+imgurl+'"}, "children":[]}'
+                print "===== POSLEDNI ======"
+            else:
+                tmp_jsn += '{"id":"'+statusid+'", "name":"'+sname+'","data": {"tsomekey":"'+text+'", "image":"'+imgurl+'"}, "children":[]},'
+                print "===== E ======"
+    
+#    jsn = simplejson.loads('{"id": "stred00", "name": "stred","data": { "some key": "some value", "some other key": "some other value"},"children": [' +tmp_jsn+ ']}')
+    tmp_jsn = tmp_jsn.encode('utf-8')
+#    jsn = '{"id": "stred00", "name": "stred","data": { "some key": "some value", "some other key": "some other value"},"children": [' +tmp_jsn+ ']}'
+    jsn = simplejson.loads('{"id": "ME", "name": "me","data": {},"children": [' +tmp_jsn+ ']}')
+    
+#    print jsn
     return jsn
 #    jsn = "..."
 #     print jsn
@@ -104,8 +138,9 @@ def replaceUrl(text):
         if (m):
 #            print "ahoj "+m.group(1)
 #            text = text.replace(word,'<a href="javascript:otevriOdkaz('+m.group(1)+')">'+m.group(1)+'</a>')
+#             text = text.replace(word,"<a href='#' onclick='Titanium.Desktop.openURL(\'"+m.group(1)+"\');'>"+m.group(1)+"</a>")
              text = text.replace(word,'<a href="#" onclick="Titanium.Desktop.openURL(\''+m.group(1)+'\');">'+m.group(1)+'</a>')
-            
+              
     return text
 
 def browserOpen(link):
